abab6489c71d68926dccaaf082398aca644a8478
Ben Mitchinson <mitchinson.dev@gmail.com>
2023-03-22 19:28:15 -0600

    Postgresql install

--- scripts/install_urbanos.sh
+++ scripts/install_urbanos.sh
@@ -7,10 +7,13 @@ echo "Is your kubeconfig up to date? ./scripts/copy_new_kube_config.sh"
 helm repo add minio https://operator.min.io/
 
 # install minio operator
-# note: trying to see if it can be in the urbanos namespace
 kubectl apply -f src/resource_additions/minio_user.yaml -n urbanos
 helm upgrade minio-operator minio/operator --install --version "4.5.8" --namespace urbanos -f src/resource_additions/minio_operator_values.yaml
 helm upgrade minio-tenant minio/tenant --install --version "4.5.8" --namespace urbanos -f src/resource_additions/minio_tenant_values.yaml
+
+# install postgres
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm upgrade postgresql bitnami/postgresql --install --version "12.1.14" --namespace urbanos -f src/resource_additions/postgres_values.yaml
 
 # install urbanos w tenant config
 helm upgrade urbanos urbanos/urban-os --install --version "1.13.31" -i -f src/urbanos_demo_chart_values.yaml -n urbanos
--- scripts/uninstall_urbanos.sh
+++ scripts/uninstall_urbanos.sh
@@ -1,6 +1,12 @@
 kubectl delete -f src/resource_additions/minio_user.yaml -n urbanos
-kc delete pvc data0-urbanos-minio-pool-0-0 -n urbanos
 
 helm uninstall urbanos -n urbanos
 helm uninstall minio-operator -n urbanos
 helm uninstall minio-tenant -n urbanos
+helm uninstall postgresql -n urbanos
+
+echo "removing minio pvc"
+kc delete pvc data0-urbanos-minio-pool-0-0 -n urbanos
+
+echo "removing postgres pvc"
+kc delete pvc data-postgresql-0 -n urbanos
